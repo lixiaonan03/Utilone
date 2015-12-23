c746c34a6eb6512116d08a4747c5ebc535549e43
lixiaonan03 <472379153@qq.com>
2015-12-23 16:33:33 +0800

    网路请求volleyutil的修改

--- AppFrame/src/main/java/android/volley/listener/HttpBackBaseListener.java
+++ AppFrame/src/main/java/android/volley/listener/HttpBackBaseListener.java
@@ -6,7 +6,7 @@ import com.android.volley.VolleyError;
  * 类名:		HttpBackStringListener
  * 描述:		通信成功后的接口回调  其中成功的方法会直接string 返回回来
  */
-public interface HttpBackStringListener {
+public interface HttpBackBaseListener {
   /**
    * 成功的方法
    * @param string
