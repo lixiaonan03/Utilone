8230739f3a191ecdc0256a50d89ee7050f68d44f
lixiaonan <472379153@qq.com>
2022-10-14 15:09:35 +0800

    设置状态栏沉浸的

--- app/src/main/java/com/lxn/utilone/activity/WelcomeActivity.java
+++ app/src/main/java/com/lxn/utilone/activity/WelcomeActivity.java
@@ -2,20 +2,29 @@ package com.lxn.utilone.activity;
 
 import android.app.Activity;
 import android.app.Instrumentation;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.os.Message;
 import android.os.SystemClock;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
 import com.alibaba.android.arouter.launcher.ARouter;
+import com.jaeger.library.StatusBarUtil;
 import com.lxn.utilone.R;
 import com.lxn.utilone.hook.InstrumentationProxy;
 import com.lxn.utilone.util.Log;
 
 import java.lang.reflect.Field;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 
 /**
  * @author lixiaonan
@@ -28,8 +37,10 @@ public class WelcomeActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_welcome);
+
+        //设置状态栏沉浸的
+        StatusBarUtil.setTranslucent(this,0);
 
         replaceActivityInstrumentation(this);
 
@@ -40,15 +51,23 @@ public class WelcomeActivity extends BaseActivity {
 //                Intent intent = new Intent(WelcomeActivity.this,MainActivity.class);
 //                startActivity(intent);
                 ARouter.getInstance().build(ActivityConstans.MAIN_PATH).navigation();
+                finish();
             }
         });
-        //
-        startWatchDog();
-        try {
-            Thread.sleep(6000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        //测试ANR 代码的
+//        startWatchDog();
+//        try {
+//            Thread.sleep(6000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        Handler handler1 = new Handler(Looper.getMainLooper());
+        Log.i("lxnHandle","hanlder1===="+ handler1.getClass().toString());
+
+        Message message = Message.obtain(handler);
+        message.what = 2;
+        handler.sendMessageDelayed(message,2000000);
+        Log.i("lxnHandle","handler===="+ handler.getClass().toString());
     }
 
     /**
@@ -98,4 +117,19 @@ public class WelcomeActivity extends BaseActivity {
         };
         anrThread.start();
     }
+
+
+    private Handler handler = new Handler(Looper.getMainLooper()){
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            try {
+                Log.i("lxnHandle","执行的===");
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    };
+
 }
