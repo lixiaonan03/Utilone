c6de121c1726c8f348157de602c6433b590d7986
lixiaonan <472379153@qq.com>
2022-11-29 17:38:32 +0800

    hook别人的app的方法使用的注释添加

--- app/src/main/java/com/lxn/utilone/activity/XposedDemo.java
+++ app/src/main/java/com/lxn/utilone/activity/XposedDemo.java
@@ -17,11 +17,28 @@ public class XposedDemo implements IXposedHookLoadPackage {
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
         Log.i("lxn12", "包名" + lpparam.packageName + "==" + lpparam.processName);
-        XposedHelpers.findAndHookMethod("android.app.Activity", lpparam.classLoader, "onResume", new XC_MethodHook() {
+//        XposedHelpers.findAndHookMethod("android.app.Activity", lpparam.classLoader, "onResume", new XC_MethodHook() {
+//            @Override
+//            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+//                super.beforeHookedMethod(param);
+//                Log.i("lxn12", "方法执行前" + param.thisObject);
+//            }
+//
+//            @Override
+//            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+//                super.afterHookedMethod(param);
+//                Log.i("lxn12", "方法执行后" + param.thisObject);
+//            }
+//        });
+
+        //FIXME： 1、hook 手机中的app的方法调用(一定要在 LSPosed APP中把需要hook的app加入到utilone的模块中)   2、启动之后在下面的方法中判断要hook的方法 注意是否有参数
+
+        XposedHelpers.findAndHookMethod("android.widget.TextView", lpparam.classLoader, "setText", CharSequence.class,new XC_MethodHook() {
             @Override
             protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
                 super.beforeHookedMethod(param);
                 Log.i("lxn12", "方法执行前" + param.thisObject);
+                Log.i("lxn121", "获取参数的" + param.args[0].toString());
             }
 
             @Override
@@ -30,6 +47,8 @@ public class XposedDemo implements IXposedHookLoadPackage {
                 Log.i("lxn12", "方法执行后" + param.thisObject);
             }
         });
+
+
 
     }
 }
